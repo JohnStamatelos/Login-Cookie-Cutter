69552d9437acde01ad67cc16ce5b898b54afa20f
Stamatelos.I <stamatelos.i@gmail.com>
2023-02-05 22:54:18 +0200

    feat(app.component): cleanup and add logout logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataStoreService } from 'src/shared/services/data-store/data-store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title = 'Login-Cookie-Cutter';
+  isLoggedIn!: boolean;
+
+  constructor(
+    private dataStoreService: DataStoreService,
+    private router: Router
+  ) {}
+
+  ngDoCheck(): void {
+    this.isLoggedIn = this.dataStoreService.user?.email ? true : false;
+  }
+
+  logout(): void {
+    this.dataStoreService.user.email = '';
+    this.router.navigate(['/login']);
+  }
 }
