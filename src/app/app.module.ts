56810b8628a0adc87f40cfa7df5d58f660025957
Stamatelos.I <stamatelos.i@gmail.com>
2023-02-05 20:42:45 +0200

    feat(login-page): add password-strength package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatPasswordStrengthModule } from '@angular-material-extensions/password-strength';
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    MatPasswordStrengthModule.forRoot(),
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
 })
 export class AppModule {}
